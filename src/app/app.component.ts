cdba2fd2251714f688fa5bec186d09d656631d11
Breyner Mola <breinerm@blumer.cc>
2023-04-19 12:16:36 -0500

    login supergraph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'agraphql';
+export class AppComponent implements OnInit {
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit() {
+    let LOGIN = gql`mutation{
+      login(input: {
+        email: "admin@admin.com",
+        password: "password"
+      }){
+        tokenAccess
+      }
+    }`;
+    this.apollo.mutate({
+      mutation: LOGIN,
+    }).subscribe(res => console.log(res))
+  }
 }
